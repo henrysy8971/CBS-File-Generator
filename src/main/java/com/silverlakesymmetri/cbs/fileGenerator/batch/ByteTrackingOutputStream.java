cdce9b666a0398309025fba23a4e59d6855ba480
rtb\henry.sy <henry.sy@silverlakeaxis.com>
2026-02-17 22:39:18 +0800

    BeanIOFormatWriter and GenericXMLWriter refactor

---
+++ src/main/java/com/silverlakesymmetri/cbs/fileGenerator/batch/ByteTrackingOutputStream.java
@@ -0,0 +1,51 @@
+package com.silverlakesymmetri.cbs.fileGenerator.batch;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class ByteTrackingOutputStream extends OutputStream {
+
+	private final OutputStream delegate;
+	private long bytesWritten = 0;
+
+	public ByteTrackingOutputStream(OutputStream delegate) {
+		this.delegate = delegate;
+	}
+
+	public ByteTrackingOutputStream(OutputStream delegate, long initialOffset) {
+		this.delegate = delegate;
+		this.bytesWritten = initialOffset;
+	}
+
+	@Override
+	public void write(int b) throws IOException {
+		delegate.write(b);
+		bytesWritten++;
+	}
+
+	@Override
+	public void write(byte[] b) throws IOException {
+		delegate.write(b);
+		bytesWritten += b.length;
+	}
+
+	@Override
+	public void write(byte[] b, int off, int len) throws IOException {
+		delegate.write(b, off, len);
+		bytesWritten += len;
+	}
+
+	public long getBytesWritten() {
+		return bytesWritten;
+	}
+
+	@Override
+	public void flush() throws IOException {
+		delegate.flush();
+	}
+
+	@Override
+	public void close() throws IOException {
+		delegate.close();
+	}
+}
